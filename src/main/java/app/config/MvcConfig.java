703f59bb9b74a9e51588a826c33d7ec670a92c7f
Tamara Ustyan <toma.ustyan@gmail.com>
2022-08-03 13:10:59 +0000

    24 front page

--- src/main/java/app/config/MvcConfig.java
+++ src/main/java/app/config/MvcConfig.java
@@ -1,15 +1,22 @@
 package app.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
         registry.addViewController("/admin").setViewName("admin");
         registry.addViewController("/manager").setViewName("manager");
         registry.addViewController("/passenger").setViewName("passenger");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/css/");
+        /*registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/");*/
     }
 }
--- src/main/java/app/repositories/PassengerRepository.java
+++ src/main/java/app/repositories/PassengerRepository.java
@@ -2,6 +2,7 @@ package app.repositories;
 
 import app.entities.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 //Репозиторий сущности "Пассажир"
 @Repository
