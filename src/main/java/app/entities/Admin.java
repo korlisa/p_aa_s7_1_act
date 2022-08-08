05913778c9dccfc59f5e915665b06a8dc94a4338
Эльвира Минибаева <minibaevaelvirar@gmail.com>
2022-08-08 08:59:53 +0000

    Added admin functionality. Created AdminRestController, adminApp.js,...

--- src/main/java/app/entities/Admin.java
+++ src/main/java/app/entities/Admin.java
@@ -1,9 +1,6 @@
 package app.entities;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
-import javax.persistence.DiscriminatorValue;
+
 import javax.persistence.Entity;
 
 
@@ -14,9 +11,8 @@ import javax.persistence.Entity;
  * @author Minibaeva Elvira
  */
 
-@Data
-@NoArgsConstructor
-@Component
+
+
 @Entity(name = "admins")
 public class Admin extends User {
 }
--- src/main/java/app/services/UserService.java
+++ src/main/java/app/services/UserService.java
@@ -1,5 +1,8 @@
 package app.services;
 
+
+import app.entities.Admin;
+import app.entities.AirlineManager;
 import app.entities.Passenger;
 import app.entities.User;
 import app.repositories.PassengerRepository;
@@ -10,6 +13,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * Сервис для User нужен для SpringSecurity
@@ -21,6 +27,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
+    private final RoleService roleService;
     private final PassengerRepository passengerRepository;
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(12);
 
@@ -30,6 +37,47 @@ public class UserService implements UserDetailsService {
     public void saveUser (User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
+    }
+
+    /**Метод сохранения пользователя в зависимости от его класса.
+     * Используется на странице admin.html при изменении пользователя.
+     *
+     * @param user
+     */
+    public void saveUserFromController(User user) {
+        if (userRepository.findUserById(user.getId()).getClass().toString().contains("Admin")) {
+            Admin admin = new Admin();
+            admin.setId(user.getId());
+            admin.setEmail(user.getEmail());
+            admin.setRoles(user.getRoles());
+            admin.setPassword(passwordEncoder.encode(user.getPassword()));
+            admin.addRoleToCollection(roleService.findRoleByName("ROLE_ADMIN"));
+            userRepository.save(admin);
+        } else {
+            AirlineManager manager = new AirlineManager();
+            manager.setId(user.getId());
+            manager.setEmail(user.getEmail());
+            manager.setRoles(user.getRoles());
+            manager.setPassword(passwordEncoder.encode(user.getPassword()));
+            manager.addRoleToCollection(roleService.findRoleByName("ROLE_MANAGER"));
+            userRepository.save(manager);
+        }
+    }
+    @Transactional(readOnly = true)
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+    @Transactional(readOnly = true)
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public User findById(Long id) {
+        return userRepository.findUserById(id);
     }
 
     /**
@@ -45,8 +93,16 @@ public class UserService implements UserDetailsService {
                 user.getRoles());
 
     }
+
+
+    public void deleteAll() {
+        userRepository.deleteAll();
+    }
+
+
     public Passenger findPassengerByEmail(String email) {
         User user = userRepository.findByEmail(email);
         return passengerRepository.findPassengerById(user.getId());
     }
+
 }
