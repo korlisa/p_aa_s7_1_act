5cbda6e4b37f372a21c191cb24cb2de10ff1e7dc
OlgaMaslova300622 <oa-maslova@ya.ru>
2022-11-06 19:13:57 +0300

    Added the entity "Destination", the controller class "DestinationRestController", the repository class "DestinationRepository", the interface "SDestinationService" and the implementation of the service "DestinationServiceImpl".

--- src/main/java/app/repositories/DestinationRepository.java
+++ src/main/java/app/repositories/DestinationRepository.java
@@ -1,5 +1,33 @@
 package app.repositories;
 
-public interface DestinationRepository {
+import app.entities.Destination;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface DestinationRepository extends JpaRepository<Destination, Long> {
+
+@Query("Select d from Destination d where d.id = :id")
+Destination getDestinationById (Long id);
+
+
+@Query("select d from Destination d where d.city = :city")
+Destination getDestinationByCity (String city);
+
+
+@Query("select d from Destination d where d.country_name = :country_name")
+Destination getDestinationByCountry_name (String country_name);
+
+
+
+
+
+
+
+
+
+
 
 }
--- src/main/java/app/services/DestinationService.java
+++ src/main/java/app/services/DestinationService.java
@@ -1,5 +1,29 @@
 package app.services;
 
+import app.entities.Destination;
+
+import java.util.List;
+
+
 public interface DestinationService {
+
+    List<Destination> getAllDestinations();
+
+    Destination saveDestination (Destination destination);
+
+    void updateDestination (Destination destination);
+
+    void deleteAllDestination();
+
+    Destination getDestinationById(Long id);
+
+    Destination getDestinationByCity (String city);
+
+    Destination getDestinationByCountry_name (String country_name);
+
+
+
+
+
 
 }
