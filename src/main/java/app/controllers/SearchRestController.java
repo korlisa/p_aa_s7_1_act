456e15a051cd43610de0730740e20b062f776502
komarov rostislav <hu4lifefllwer@gmail.com>
2022-12-16 12:16:17 +0300

    PAS1-30 implemented Search, SearchRoute, SearchResult, SearchRestController, Search Service and Repo

--- src/main/java/app/controllers/SearchRestController.java
+++ src/main/java/app/controllers/SearchRestController.java
@@ -1,105 +1,132 @@
-/*
+
 package app.controllers;
 
 
 import app.entities.Search;
 import app.services.SearchService;
-import app.util.JsonParser;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.tomcat.util.json.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
-*/
+
 /**
  * REST controllers for Search entity
- *
- * @author Eugene Kolyshev
+ * @author Komarov Rostislav
  * @see Search
- *//*
+ */
 
 
 @Api(value = "Search Rest Controller", tags = "search")
 @RestController
-@RequestMapping("api/search")
+@RequestMapping("/api/search")
 public class SearchRestController {
 
     private final SearchService searchService;
-    private final JsonParser jsonParser;
 
     @Autowired
-    public SearchRestController(SearchService searchService, JsonParser jsonParser) {
+    public SearchRestController(SearchService searchService) {
         this.searchService = searchService;
-        this.jsonParser = jsonParser;
     }
 
-    */
-/**
-     * Method getSearchResult() to get all SearchResult
-     *
-     * @return List of Search, OK
-     *//*
+    /**
+     * <p><em>getAllSearch</em> self explained - getting all entities of {@link app.entities.Search} in list</p>
+     * @return returns 200 status and list of every search
+     * or 204 No Content status if nothing found
+     */
 
-
-    @ApiOperation(value = "Get all Search")
-    @GetMapping()
-    private ResponseEntity<List<Search>> getAllSearch() {
-        List<Search> search = searchService.getAll();
-        return search != null && !search.isEmpty()
-                ? new ResponseEntity(search, HttpStatus.OK)
-                : new ResponseEntity(HttpStatus.NOT_FOUND);
+    @ApiOperation(value = "Get every search")
+    @GetMapping
+    public ResponseEntity<List<Search>> getAllSearch() {
+        List<Search> search = searchService.getAllSearches();
+        if(search.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } else {
+            return ResponseEntity.ok().body(search);
+        }
     }
 
-    */
-/**
-     * Method createSearch() to create new Search
-     *
-     * @return search, OK
-     *//*
+    /**
+     * <p><em>createSearch</em> creating {@link app.entities.Search} class from request body and putting into database</p>
+     * @param search {@link app.entities.Search} class from request body
+     * @return returns 200 status and object itself in response body if added successfully
+     * or 400 bad request status and object in response body if something goes wrong
+     */
 
     @ApiOperation(value = "Create Search")
-    @PostMapping()
-    private ResponseEntity<?> createSearch(@RequestBody String jsonRoute) throws org.json.simple.parser.ParseException {
-        Search search = jsonParser.getSearchByJson(jsonRoute);
-        searchService.save(search);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    @PostMapping
+    public ResponseEntity<Search> createSearch(@RequestBody Search search) {
+        try {
+            searchService.createSearch(search);
+            return ResponseEntity.ok(search);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(search);
+        }
     }
 
-    */
-/**
-     * Method getSearch() to get Search by id
-     *
-     * @return search, OK
-     *//*
+    /**
+     * <p><em>getSearch</em> getting desired {@link app.entities.Search} element by id</p>
+     * @param id Id of desired element
+     * @return 200 status and object if found
+     * or 404 not found if object isn't exist
+     */
 
-    @ApiOperation(value = "Get Search by id")
+    @ApiOperation(value = "Get search by id")
     @GetMapping("{id}")
-    private ResponseEntity<?> getSearch(@PathVariable("id") Long id) {
-        final Search search = searchService.getById(id);
-        return search != null
-                ? new ResponseEntity<>(search, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<Search> getSearch(@PathVariable("id") Long id) {
+        Optional<Search> target = searchService.getSearch(id);
+        if(target.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } else {
+            return ResponseEntity.ok(target.get());
+        }
     }
 
-    */
-/**
-     * Method updateSearch() to update Search
-     *
-     * @return search, OK
-     *//*
+    /**
+     * <p><em>updateSearch</em> updating object in database with same primary key(id)</p>
+     * @param search updated {@link app.entities.Search} class from request body
+     * @return status 200 OK and updated object itself
+     * or 400 bad request and object if something goes wrong
+     */
 
-    @ApiOperation(value = "Update Search")
-    @PutMapping()
-    private ResponseEntity<?> updateSearch(@RequestBody Search search) {
-        //searchService.save(search);
-        return new ResponseEntity<>(search, HttpStatus.OK);
+    @ApiOperation(value = "Update search")
+    @PutMapping
+    public ResponseEntity<Search> updateSearch(@RequestBody Search search) {
+        try {
+            searchService.updateSearch(search);
+            return ResponseEntity.ok(search);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(search);
+        }
+    }
+
+    /**
+     * <p><em>deleteSearch</em> deleting desired element from database</p>
+     * @param id Id of desired element
+     * @return status 200 OK and deleted object itself
+     * or 404 Not Found if desired object isn't exist
+     */
+
+    @ApiOperation(value = "Delete search")
+    @DeleteMapping("{id}")
+    public ResponseEntity<Search> deleteSearch(@PathVariable Long id) {
+        try {
+            Optional<Search> target = searchService.getSearch(id);
+            searchService.deleteSearch(id);
+            return ResponseEntity.ok(target.get());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.notFound().build();
+        }
     }
 
 
 }
-*/
+
--- src/main/java/app/services/SearchService.java
+++ src/main/java/app/services/SearchService.java
@@ -1,5 +1,14 @@
 package app.services;
 
-public interface SearchService {
+import app.entities.Search;
 
+import java.util.List;
+import java.util.Optional;
+
+public interface SearchService {
+    void createSearch(Search search);
+    Search deleteSearch(Long id);
+    Search updateSearch(Search search);
+    Optional<Search> getSearch(Long id);
+    List<Search> getAllSearches();
 }
