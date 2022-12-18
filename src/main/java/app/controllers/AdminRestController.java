3642d8839f7d97cd39bc9e92f60687e6a8b4ca7b
Komarov <hu4lifefllwer@gmail.com>
2022-12-18 10:30:55 +0300

    impl-ed tests for all endpoints in AdminRestController

--- src/main/java/app/controllers/AdminRestController.java
+++ src/main/java/app/controllers/AdminRestController.java
@@ -2,18 +2,18 @@ package app.controllers;
 
 
 /**
- * RestController класса Admin
+ * RestController класса Admin "api/admin"
  * getCurrentUser
- * findAllUsers
- * findUserById
- * create Admin
- * createManager
- * updateUser
- * deleteUser
- * findAllRoles
- * createRole
- * findRoleById
- * deleteRole
+ * findAllUsers GET "/users"
+ * findUserById GET "/users/{id}"
+ * create Admin POST "/"
+ * createManager POST "/manager"
+ * updateUser PUT "/users/{id}"
+ * deleteUser DELETE "/users/{id}"
+ * findAllRoles GET "/roles"
+ * createRole POST "/roles"
+ * findRoleById GET "/roles/{id}"
+ * deleteRole DELETE "/roles/{id}"
  *
  * @author Minibaeva Elvira
  */
